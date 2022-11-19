66583c801174b3070f9a8ad79dad56d4aaab0ef7
Guilherme Menezes <guilhermeladins@gmail.com>
2022-11-18 21:47:40 -0300

    commands: utils: added userinfo command

--- src/utils/time.rs
+++ src/utils/time.rs
@@ -29,4 +29,6 @@ impl fmt::Display for HumanDate {
     }
 }
 
-pub fn get_discord_relative_time(t: i64) -> String { format!("<t:{t}:R>") }
+pub fn get_discord_relative_time(t: i64) -> String {
+    format!("<t:{t}:R>")
+}
